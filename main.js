7d2b58ef46c3991b18a2672c9b58ca0a8ec32670
Mouna-Bouzid <bouzid.mouna@outlook.com>
2020-04-05 18:07:51 +0100

    final touch css

--- main.js
+++ main.js
@@ -38,9 +38,6 @@ closeModal.addEventListener('click', function(){
   modal.style.display = "none";
 })
 
-  // closeModal.onclick = function() {
-  //   modal.style.display = "none";
-  // }
 window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
